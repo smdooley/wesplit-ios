957a32913592fcdc20d1ddacb64644a2540a7c93
Sean Dooley <s.dooley@ctidigital.com>
2024-10-28 13:39:27 +0000

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sean Dooley on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
